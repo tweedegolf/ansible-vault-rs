48739661f663717524fd02521adf17b568e5ab1e
Eric Trombly <etrombly@yahoo.com>
2021-02-24 12:16:29 -0600

    update dependencies and add encryption support

--- src/lib.rs
+++ src/lib.rs
@@ -5,88 +5,54 @@
 //! It detects incorrect vault secrets and incorrectly formatted vaults,
 //! and yields the appropriate errors.
 
+use aes_ctr::cipher::{NewStreamCipher, SyncStreamCipher};
+use block_padding::{Padding, Pkcs7};
+use hmac::{Hmac, Mac, NewMac};
+use pbkdf2::pbkdf2;
+use rand::{thread_rng, Rng};
+use sha2::Sha256;
+use std::io::BufRead;
+use thiserror::Error;
+
 /// The error type for decrypting Ansible vaults.
 ///
 /// Errors either originate from failing I/O operations, or from
 /// passing incorrect (formatted) files, streams or secrets.
-#[derive(Debug)]
-pub enum Error {
-    IoError(std::io::Error),
+#[derive(Error, Debug)]
+pub enum VaultError {
+    #[error("IO error")]
+    IoError(#[from] std::io::Error),
+    #[error("Error encoding UTF-8")]
+    Utf8Error(#[from] std::str::Utf8Error),
+    #[error("Error padding input")]
+    PaddingError,
+    #[error("file is not an ansible vault")]
     NotAVault,
+    #[error("file is a broken ansible vault")]
     InvalidFormat,
+    #[error("incorrect secret for ansible vault")]
     IncorrectSecret,
 }
 
-/// A specialized `Result` type for decrypting Ansible vaults.
-pub type Result<T> = std::result::Result<T, Error>;
-
-impl std::cmp::PartialEq for Error {
-    fn eq(&self, other: &Error) -> bool {
-        match (self, other) {
-            (Error::IoError(_), Error::IoError(_)) => true,
-            (Error::NotAVault, Error::NotAVault) => true,
-            (Error::InvalidFormat, Error::InvalidFormat) => true,
-            (Error::IncorrectSecret, Error::IncorrectSecret) => true,
-            _ => false,
-        }
-    }
-}
-
-impl std::fmt::Display for Error {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        match self {
-            Error::IoError(err) => err.fmt(f),
-            _ => {
-                use std::error::Error;
-                write!(f, "{}", self.description())
-            }
-        }
-    }
-}
-
-impl std::error::Error for Error {
-    fn description(&self) -> &str {
-        match self {
-            Error::IoError(err) => err.description(),
-            Error::NotAVault => "file is not an ansible vault",
-            Error::InvalidFormat => "file is a broken ansible vault",
-            Error::IncorrectSecret => "incorrect secret for ansible vault",
-        }
-    }
-
-    fn cause(&self) -> Option<&std::error::Error> {
-        match self {
-            Error::IoError(err) => Some(err),
-            _ => None,
-        }
-    }
-}
-
-impl From<std::io::Error> for Error {
-    fn from(error: std::io::Error) -> Self {
-        Error::IoError(error)
-    }
-}
-
-impl From<std::string::FromUtf8Error> for Error {
+impl From<std::string::FromUtf8Error> for VaultError {
     fn from(_error: std::string::FromUtf8Error) -> Self {
-        Error::InvalidFormat
+        VaultError::InvalidFormat
     }
 }
 
-impl From<base16::DecodeError> for Error {
+impl From<base16::DecodeError> for VaultError {
     fn from(_error: base16::DecodeError) -> Self {
-        Error::InvalidFormat
+        VaultError::InvalidFormat
     }
 }
 
-impl From<hmac::crypto_mac::InvalidKeyLength> for Error {
+impl From<hmac::crypto_mac::InvalidKeyLength> for VaultError {
     fn from(_error: hmac::crypto_mac::InvalidKeyLength) -> Self {
-        Error::InvalidFormat
+        VaultError::InvalidFormat
     }
 }
 
-fn read_hex_lines<T: std::io::BufRead>(lines: std::io::Lines<T>) -> Result<Vec<u8>> {
+fn read_hex_lines<T: std::io::BufRead>(lines: std::io::Lines<T>) -> Result<Vec<u8>, VaultError> {
     let mut buffer: Vec<u8> = vec![];
 
     let mut i = 0;
@@ -105,79 +71,126 @@ fn read_hex_lines<T: std::io::BufRead>(lines: std::io::Lines<T>) -> Result<Vec<u
 }
 
 /// See https://github.com/ansible/ansible/blob/devel/lib/ansible/parsing/vault/__init__.py#L1286.
-fn verify_vault(key: &[u8], ciphertext: &[u8], crypted_hmac: &[u8]) -> Result<bool> {
-    use hmac::Mac;
+fn verify_vault(key: &[u8], ciphertext: &[u8], crypted_hmac: &[u8]) -> Result<bool, VaultError> {
+    let mut hmac = Hmac::<Sha256>::new_varkey(key)?;
+    hmac.update(&ciphertext);
+    let result = hmac.finalize();
 
-    let mut hmac = hmac::Hmac::<sha2::Sha256>::new_varkey(key)?;
-    hmac.input(&ciphertext);
-
-    Ok(hmac.result().code().as_slice().eq(crypted_hmac)) // Constant time equivalence is not required for this use case.
+    Ok(result.into_bytes().as_slice().eq(crypted_hmac)) // Constant time equivalence is not required for this use case.
 }
 
 /// Decrypt an ansible vault stream using a key.
 ///
 /// When succesful, yields a plaintext byte buffer.
-pub fn read_vault<T: std::io::Read>(input: T, key: &str) -> Result<Vec<u8>> {
-    use block_padding::{Padding, Pkcs7};
-    use std::io::BufRead;
-
+pub fn read_vault<T: std::io::Read>(input: T, key: &str) -> Result<Vec<u8>, VaultError> {
     let mut lines = std::io::BufReader::new(input).lines();
-    let first: String = lines.next().ok_or(Error::NotAVault)??;
+    let first: String = lines.next().ok_or(VaultError::NotAVault)??;
 
     if first != "$ANSIBLE_VAULT;1.1;AES256" {
-        return Err(Error::NotAVault);
+        return Err(VaultError::NotAVault);
     }
 
     let inner = String::from_utf8(read_hex_lines(lines)?)?;
 
     let mut lines = inner.lines();
 
-    let salt = base16::decode(&lines.next().ok_or(Error::InvalidFormat)?)?;
-    let hmac_verify = base16::decode(&lines.next().ok_or(Error::InvalidFormat)?)?;
-    let mut ciphertext = base16::decode(&lines.next().ok_or(Error::InvalidFormat)?)?;
+    let salt = base16::decode(&lines.next().ok_or(VaultError::InvalidFormat)?)?;
+    let hmac_verify = base16::decode(&lines.next().ok_or(VaultError::InvalidFormat)?)?;
+    let mut ciphertext = base16::decode(&lines.next().ok_or(VaultError::InvalidFormat)?)?;
 
     let mut hmac_buffer = [0; 80];
-    pbkdf2::pbkdf2::<hmac::Hmac<sha2::Sha256>>(key.as_bytes(), &salt, 10000, &mut hmac_buffer);
+    pbkdf2::<Hmac<Sha256>>(key.as_bytes(), &salt, 10000, &mut hmac_buffer);
 
     let key1 = &hmac_buffer[0..32];
     let key2 = &hmac_buffer[32..64];
     let iv = &hmac_buffer[64..80];
 
     if !verify_vault(key2, &ciphertext, &hmac_verify)? {
-        return Err(Error::IncorrectSecret);
+        return Err(VaultError::IncorrectSecret);
     }
 
-    use aes_ctr::stream_cipher::{NewStreamCipher, SyncStreamCipher};
-    let mut cipher = aes_ctr::Aes256Ctr::new_var(key1, iv).map_err(|_err| Error::InvalidFormat)?;
+    let mut cipher =
+        aes_ctr::Aes256Ctr::new_var(key1, iv).map_err(|_err| VaultError::InvalidFormat)?;
 
     cipher.apply_keystream(&mut ciphertext);
     let n = Pkcs7::unpad(&ciphertext)
-        .map_err(|_| Error::InvalidFormat)?
+        .map_err(|_| VaultError::InvalidFormat)?
         .len();
     ciphertext.truncate(n);
 
     Ok(ciphertext)
 }
 
+pub fn write_vault(input: &mut [u8], key: &str) -> Result<String, VaultError> {
+    let mut rng = thread_rng();
+    let salt: [u8; 32] = rng.gen();
+
+    let mut hmac_buffer = [0; 80];
+    pbkdf2::<Hmac<Sha256>>(key.as_bytes(), &salt, 10000, &mut hmac_buffer);
+
+    let key1 = &hmac_buffer[0..32];
+    let key2 = &hmac_buffer[32..64];
+    let iv = &hmac_buffer[64..80];
+
+    let mut cipher =
+        aes_ctr::Aes256Ctr::new_var(key1, iv).map_err(|_err| VaultError::InvalidFormat)?;
+
+    let n = input.len();
+    let offset = (n % 128) * 8;
+    let mut buffer = input.to_vec();
+    buffer.resize(n + offset, 0);
+    let n = Pkcs7::pad(&mut buffer, n, 128)
+        .map_err(|_err| VaultError::PaddingError)?
+        .len();
+    buffer.truncate(n);
+
+    cipher.apply_keystream(&mut buffer);
+
+    let mut hmac = Hmac::<Sha256>::new_varkey(key2)?;
+    hmac.update(&buffer);
+    let hmac_verify = hmac.finalize();
+
+    let mut output = vec!["$ANSIBLE_VAULT;1.1;AES256".as_bytes()];
+    let salt = base16::encode_upper(&salt);
+    let hmac_verify = base16::encode_upper(&hmac_verify.into_bytes());
+    let lines = format!(
+        "{}\n{}\n{}",
+        salt,
+        hmac_verify,
+        base16::encode_upper(&buffer)
+    );
+    let mut lines = base16::encode_upper(&lines).as_bytes().to_owned();
+    for line in lines.chunks_mut(80) {
+        output.push(line);
+    }
+    Ok(std::string::String::from_utf8(
+        output.join("\n".as_bytes()),
+    )?)
+}
+
 /// Decrypt an ansible vault file using a key.
 ///
 /// When succesful, yields a plaintext byte buffer.
-pub fn read_vault_from_file(path: &std::path::Path, key: &str) -> Result<Vec<u8>> {
+pub fn read_vault_from_file(path: &std::path::Path, key: &str) -> Result<Vec<u8>, VaultError> {
     let f = std::fs::File::open(path)?;
     read_vault(f, key)
 }
 
 #[cfg(test)]
 mod tests {
+    use std::{io::Read, str::FromStr};
+
     fn lipsum_path() -> std::path::PathBuf {
-        use std::str::FromStr;
         std::path::PathBuf::from_str("./test/lipsum.vault").unwrap()
     }
 
     #[test]
     fn wrong_password() {
         let result = crate::read_vault_from_file(&lipsum_path(), "not shibboleet").unwrap_err();
-        std::assert_eq!(result, crate::Error::IncorrectSecret);
+        match result {
+            crate::VaultError::IncorrectSecret => {}
+            _ => panic!(),
+        }
     }
 
     #[test]
@@ -186,5 +199,17 @@ mod tests {
         let lipsum = std::string::String::from_utf8(buf).unwrap();
         let reference = std::fs::read_to_string("./test/lipsum.txt").unwrap();
         std::assert_eq!(lipsum, reference);
+    }
+
+    #[test]
+    fn encrypt() {
+        let reference = std::fs::read_to_string("./test/lipsum.txt").unwrap();
+        let mut bytes = std::fs::read("./test/lipsum.txt").unwrap();
+        let lipsum = crate::write_vault(&mut bytes, "shibboleet").unwrap();
+        let unencrypted = std::string::String::from_utf8(
+            crate::read_vault(lipsum.as_bytes(), "shibboleet").unwrap(),
+        )
+        .unwrap();
+        std::assert_eq!(unencrypted, reference);
     }
 }
